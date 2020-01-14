d90ec9937e7bf3b8a0093d93bf2ff3c3765e2d57
Yondon Fu <yondon.fu@gmail.com>
2020-01-13 22:35:12 -0500

    Pass in network as arg

--- index.js
+++ index.js
@@ -2,24 +2,35 @@ const abi = require("./JobsManager.json")
 const ethers = require("ethers")
 const utils = ethers.utils
 
+const NET_CONFIGS = {
+  mainnet: {
+    jobsManager: "0xbf07ff45f14c9ff0571b9fbdc7e2b62d29931224",
+    fromBlock: 5533890
+  },
+  rinkeby: {
+    jobsManager: "0x0A209545bb10bC2F78713a2699795ECfEa76762D",
+    fromBlock: 1771513
+  }
+}
+
 const main = async () => {
-  // MAINNET
-  // const provider = ethers.getDefaultProvider()
-  // RINKEBY
-  const provider = ethers.getDefaultProvider("rinkeby")
-  // JobsManager proxy address
-  // MAINNET
-  // const addr = "0xbf07ff45f14c9ff0571b9fbdc7e2b62d29931224"
-  // RINKEBY
-  const addr = "0x0A209545bb10bC2F78713a2699795ECfEa76762D"
-  const contract = new ethers.Contract(addr, abi, provider)
+  const net = process.argv[2]
+  const cfg = NET_CONFIGS[net]
+
+  let provider
+  if (net === "mainnet") {
+    provider = ethers.getDefaultProvider()
+  } else if (net === "rinkeby") {
+    provider = ethers.getDefaultProvider("rinkeby")
+  } else {
+    throw new Error("unknown network")
+  }
+
+  const contract = new ethers.Contract(cfg.jobsManager, abi, provider)
 
   let filter = contract.filters.Deposit(null, null)
   // Deployment block of Controller
-  // MAINNET
-  // filter.fromBlock = 5533890
-  // RINKEBY
-  filter.fromBlock = 1771513
+  filter.fromBlock = cfg.fromBlock
   filter.toBlock = "latest"
 
   let depositors = {}
@@ -48,7 +59,6 @@ const main = async () => {
     totalDeposits = totalDeposits.add(deposit)
 
     console.log(`Address: ${depositor} Amount: ${deposit}`)
-
   }
 
   console.log(`# Depositors: ${numDepositors}`)
